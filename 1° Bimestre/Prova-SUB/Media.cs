d8394374608269d5d8ccae39a21c2949a0b9475b
Izadora Martins da Silva <126421583+Izadora-Martins@users.noreply.github.com>
2023-05-09 15:10:16 -0300

    Add files via upload

---
+++ 1° Bimestre/Prova-SUB/Media.cs
@@ -0,0 +1,7 @@
+namespace Programa;
+class Media{
+     public static double Calculo(double nota1, double nota2, double nota3){
+        double result= double.NaN;
+        result = (nota1 + nota2 + nota3) / 3;
+        return result;}
+}
